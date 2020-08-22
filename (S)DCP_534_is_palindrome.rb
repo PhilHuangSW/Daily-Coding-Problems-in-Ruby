68ad77ae33c58028fe0847631239818a9645b53e
Philip Huang <philip.sw.huang@gmail.com>
2020-08-21 17:55:10 -0700

    Added problem descriptions

--- (S)DCP_534_is_palindrome.rb
+++ (S)DCP_534_is_palindrome.rb
@@ -1,4 +1,13 @@
 ############################ PALINDROME ############################
+# This problem was asked by Amazon.
+
+# Given a string, determine whether any permutation of it is a palindrome.
+
+# For example, carrace should return true, since it can be rearranged to form racecar, 
+# which is a palindrome. daily should return false, since there's no rearrangement 
+# that can form a palindrome.
+
+
 print "Input a string: "
 string = gets.chomp
 h = Hash.new{|h,k| h[k] = 0}
---
+++ (S)DCP_537_Collatz_number.rb
@@ -0,0 +1,30 @@
+############################ COLLATZ NUMBER ############################
+
+# This problem was asked by Apple.
+
+# A Collatz sequence in mathematics can be defined as follows. Starting 
+# with any positive integer:
+
+# if n is even, the next number in the sequence is n / 2
+# if n is odd, the next number in the sequence is 3n + 1
+# It is conjectured that every such sequence eventually reaches the number 1. 
+# Test this conjecture.
+
+# Bonus: What input n <= 1000000 gives the longest sequence?
+
+print "Input an integer: "
+num = gets.chomp.to_i
+count = 0
+while num != 1
+    if (num%2 == 0)
+        #num = num / 2
+        puts "Even: #{num = num/2}"
+    else
+        #num = 3*num + 1
+        puts "Odd: #{num = 3*num + 1}"
+    end
+    count += 1
+end
+
+puts "Number of times in sequence: #{count}"
+
